9e1a466558c356e8daa9be96b06f849ee04c29d1
Andrew Atwong <andrew.atwong@accenture.com>
2016-12-07 15:36:02 -0800

    finished chapter 3

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms'; //imported this for two-way binding on form inputs!
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [
+    BrowserModule,
+    FormsModule
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
